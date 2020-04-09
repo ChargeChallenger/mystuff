0b688fda60a36997742696e887e1430efff94150
datmcl <mixail.bokov9@gmail.com>
2020-04-10 00:13:47 +0700

    Added client form

--- NailsApp/Project1.dpr
+++ NailsApp/Project1.dpr
@@ -4,7 +4,8 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Unit1 in 'Unit1.pas' {FormEntering},
-  Unit2 in 'Unit2.pas' {FormRegistration};
+  Unit2 in 'Unit2.pas' {FormRegistration},
+  Unit3 in 'Unit3.pas' {FormClient};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.Initialize;
   Application.CreateForm(TFormEntering, FormEntering);
   Application.CreateForm(TFormRegistration, FormRegistration);
+  Application.CreateForm(TFormClient, FormClient);
   Application.Run;
 end.
